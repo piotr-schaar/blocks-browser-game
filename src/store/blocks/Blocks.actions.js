9701cd1b8b4c926d69a5a3a2150d783afa13c207
Piotr Schaar <piotr.schaar@gmail.com>
2019-12-03 15:12:41 +0100

    comments added

--- src/store/blocks/Blocks.actions.js
+++ src/store/blocks/Blocks.actions.js
@@ -37,12 +37,13 @@ export const createBlocks = () => dispatch => {
 };
 
 const checkMatchingDirections = (board, r, c) => {
+  // defines direction and set undefined for blocks placed up on the edges
   const top = board[r - 1] !== undefined && { row: r - 1, column: c };
   const bottom = board[r + 1] !== undefined && { row: r + 1, column: c };
   const left = board[r][c - 1] !== undefined && { row: r, column: c - 1 };
   const right = board[r][c + 1] !== undefined && { row: r, column: c + 1 };
 
-  // filter for edge blocks and finding match color
+  // filter for edge blocks and find match color
   const directionsWithMatches = [top, bottom, left, right]
     .filter(dir => dir instanceof Object)
     .filter(({ row, column }) => board[row][column].color === board[r][c].color);
@@ -57,6 +58,7 @@ export const checkAllBlocksForPossibleMatches = () => (dispatch, getState) => {
   let isPossibleMatches = false;
 
   const checkDirectionsToFindAnyPossibleMatches = () => {
+    // iteration after each block to check if it contains any matches
     for (let j = 0; j < rows; j += 1) {
       for (let i = 0; i < columns; i += 1) {
         if (checkMatchingDirections(blocks, j, i).length > 0) {
@@ -106,14 +108,19 @@ const updateBlocksWithColors = (blocks, allMatches) => {
 
 export const checkBoxesMatches = (y, x) => (dispatch, getState) => {
   let allMatchingBlocks = [];
+
   const findMatchingAndTouching = (board, r, c) => {
+    // first check after every function call that allMatchingBlocks contains already all matches
     if (allMatchingBlocks.filter(({ row, column }) => row === r && column === c).length) return;
 
-    const currentCheckingBlock = { row: r, column: c };
+    // define currentblock and get matching directions
+    const blockCurrentlyBeingChecked = { row: r, column: c };
     const directionsWithMatches = checkMatchingDirections(board, r, c);
 
+    // when current checking block has matches then add to allMatchingBlocks.
+    // map matches to find another matches by recursive function
     if (directionsWithMatches.length) {
-      allMatchingBlocks.push(currentCheckingBlock);
+      allMatchingBlocks.push(blockCurrentlyBeingChecked);
       directionsWithMatches.map(({ row, column }) => findMatchingAndTouching(board, row, column));
     }
   };
@@ -124,10 +131,10 @@ export const checkBoxesMatches = (y, x) => (dispatch, getState) => {
 
   findMatchingAndTouching(blocks, y, x);
 
+  // remove duplicates
   allMatchingBlocks = allMatchingBlocks.filter(
     (v, i, a) => a.findIndex(t => t.row === v.row && t.column === v.column) === i,
   );
-
   dispatch({
     type: POINT_TRIGGER,
     payload: allMatchingBlocks,
@@ -145,7 +152,6 @@ export const checkBoxesMatches = (y, x) => (dispatch, getState) => {
     type: UPDATE_BOARD,
     payload: boardWithUpdatedBlocks,
   });
-
   dispatch({
     type: DELETE_CLICKED_BLOCKS,
     payload: allMatchingBlocks,
